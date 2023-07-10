988bf7361973133f2c235ffaaf8e0c6bb787e10a
younes <elhakimyyouness4@gmail.com>
2023-07-10 09:42:00 +0100

    task3

--- New folder/app/src/main/java/com/example/efm/MainActivity.java
+++ New folder/app/src/main/java/com/example/efm/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.efm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
         adapter.add("Squere");
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numberText = number.getText().toString();
+
+                try {
+                    int numberValue = Integer.parseInt(numberText);
+                    if (TextUtils.isEmpty(numberText)) {
+                    throw new Exception();
+                    }
+
+                } catch (Exception e) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Caution")
+                            .setMessage("You chould enter number value")
+                            .create()
+                            .show();
+                }
+                }
+
+        });
 
 
     }
