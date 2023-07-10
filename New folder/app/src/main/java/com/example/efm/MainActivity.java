7d41dc072d00bbe492bc59757b7b8a2838a7ebdb
younes <elhakimyyouness4@gmail.com>
2023-07-10 09:23:31 +0100

    task2

--- New folder/app/src/main/java/com/example/efm/MainActivity.java
+++ New folder/app/src/main/java/com/example/efm/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.efm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    EditText number;
+    Spinner spinner;
+    Button calculate;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number = findViewById(R.id.etnumber);
+        spinner = findViewById(R.id.spinner);
+        calculate = findViewById(R.id.btncalculate);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.add("opposite");
+        adapter.add("Absolute Value");
+        adapter.add("Squere");
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
